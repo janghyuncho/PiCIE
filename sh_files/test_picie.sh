9446791459b02d125389f02aa82d0a15e480605c
Jang Hyun Cho <jc88964@cs.utexas.edu>
2021-07-02 04:16:02 -0500

    setting save root

--- sh_files/test_picie.sh
+++ sh_files/test_picie.sh
@@ -1,8 +1,11 @@
-EVAL_PATH='picie.pth.tar' # Your checkpoint directory. 
+EVAL_PATH='../../picie.pkl' # Your checkpoint directory. 
+
+mkdir -p results/test/picie/
 
 python train_picie.py \
 --data_root datasets/coco/ \
 --eval_only \
+--save_root results/test/picie \
 --eval_path ${EVAL_PATH} \
 --stuff --thing  \
 --res 320 
