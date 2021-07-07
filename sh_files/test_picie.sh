5230f86ea754672718dfd63753b6396db07ecb47
Jang Hyun Cho <jc88964@cs.utexas.edu>
2021-07-07 03:39:13 -0500

    training and testing commands

--- sh_files/test_picie.sh
+++ sh_files/test_picie.sh
@@ -1,11 +1,11 @@
 EVAL_PATH='../../picie.pkl' # Your checkpoint directory. 
 
-mkdir -p results/test/picie/
+mkdir -p results/picie/test/
 
 python train_picie.py \
 --data_root datasets/coco/ \
 --eval_only \
---save_root results/test/picie \
+--save_root results/picie/test/ \
 --eval_path ${EVAL_PATH} \
 --stuff --thing  \
 --res 320 
